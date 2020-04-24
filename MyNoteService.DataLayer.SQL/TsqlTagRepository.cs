064c0a40e89dae5a7b0190d07fbed6e9151fabb9
FreeWind94 <shukin-nikita@mail.ru>
2020-04-24 15:12:30 +0300

    TsqlTagRepository

--- MyNoteService.DataLayer.SQL/TsqlTagRepository.cs
+++ MyNoteService.DataLayer.SQL/TsqlTagRepository.cs
@@ -49,22 +49,111 @@ namespace MyNoteService.DataLayer.SQL
 
         public void DeleteEntity(int id)
         {
-            throw new NotImplementedException();
+            // соединение с базой (внутри блока using, чтобы потом она сама закрылась)
+            using (var sql = new SqlConnection(_connectionString))
+            {
+                sql.Open();
+
+                //SQL команда для удаления тега
+                using (var command = sql.CreateCommand())
+                {
+                    command.CommandText = "delete from [dbo].[Tags] where [TagID] = @tagID";
+                    command.Parameters.AddWithValue("@tagID", id);
+                    command.ExecuteNonQuery();
+                }
+            }
         }
 
+        /// <summary>
+        /// Изменяется только имя тега (по его ID)
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public Tag EditEntity(Tag item)
         {
+            // соединение с базой (внутри блока using, чтобы потом она сама закрылась)
+            using (var sql = new SqlConnection(_connectionString))
+            {
+                sql.Open();
+
+                //SQL команда для изменения имени тега
+                using (var command = sql.CreateCommand())
+                {
+                    // "пока" сделаем по простому без проверок)
+                    command.CommandText = "update [dbo].[Tags] set [TagName] = @tagName where [TagID] = @tagID";
+                    command.Parameters.AddWithValue("@tagName", item.TagName);
+                    command.Parameters.AddWithValue("@tagID", item.TagID);
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            return item;
             throw new NotImplementedException();
         }
 
         public IEnumerable<Tag> GetEntities()
         {
-            throw new NotImplementedException();
+            // соединение с базой (внутри блока using, чтобы потом она сама закрылась)
+            using (var sql = new SqlConnection(_connectionString))
+            {
+                sql.Open();
+
+                //SQL команда вывода всех тегов
+                using (var command = sql.CreateCommand())
+                {
+                    command.CommandText = "select [TagID], [TagName] from [dbo].[Tags]";
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Tag tag = new Tag
+                            {
+                                TagID = reader.GetInt32(reader.GetOrdinal("TagID")),
+                                TagName = reader.GetString(reader.GetOrdinal("TagName"))
+                            };
+
+                            yield return tag;
+                        }
+                    }
+                }
+            }
         }
 
         public Tag GetEntityByID(int id)
         {
-            throw new NotImplementedException();
+            // соединение с базой (внутри блока using, чтобы потом она сама закрылась)
+            using (var sql = new SqlConnection(_connectionString))
+            {
+                sql.Open();
+
+                //SQL команда вывода всех тегов
+                using (var command = sql.CreateCommand())
+                {
+                    command.CommandText = "select [TagID], [TagName] from [dbo].[Tags] where [TagID] = @tagID";
+                    command.Parameters.AddWithValue("@tagID", id);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        return ReadTag(reader);
+                    }
+                }
+            }
+        }
+
+        private static Tag ReadTag(SqlDataReader reader)
+        {
+            if (!reader.Read())
+            {
+                throw new IOException("Insert query did not return Tag");
+            }
+            Tag tag = new Tag
+            {
+                TagID = reader.GetInt32(reader.GetOrdinal("TagID")),
+                TagName = reader.GetString(reader.GetOrdinal("TagName"))
+            };
+
+            return tag;
         }
     }
 }
